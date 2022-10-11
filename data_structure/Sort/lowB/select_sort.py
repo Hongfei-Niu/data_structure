1c1cef98ec4f59e120d8cf01557bd742b70cac20
牛鸿飞 <niuhongfei@wuzhi-ai.com>
2022-10-11 11:22:11 +0800

    update gitignore

--- data_structure/Sort/lowB/select_sort.py
+++
@@ -1,8 +0,0 @@
-def select_sort(li):
-    for i in range(len(li) - 1):
-        min_loc = i
-        for j in range(i, len(li)):
-            if li[j] < li[min_loc]:
-                min_loc = j
-        li[i], li[min_loc] = li[min_loc], li[i]
-    return li 
